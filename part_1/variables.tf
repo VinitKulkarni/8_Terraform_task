8f88559d2d4795ab8a3b649e74a1965a60a87f14
VinitKulkarni <42830279+VinitKulkarni@users.noreply.github.com>
2025-06-02 13:56:12 +0530

    variables declared

---
+++ part_1/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type = string
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "my-access-key" {
+  type = string
+}
+
+variable "my-secret_key" {
+  type = string
+}
